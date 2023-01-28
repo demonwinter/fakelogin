fde021521b9ef993779b146dfc6cd96db8d6aef2
demonwinter <123834556+demonwinter@users.noreply.github.com>
2023-01-28 20:05:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var btnLogin = document.getElementById('do-login');
-var idLogin = document.getElementById('login');
-var username = document.getElementById('username');
-btnLogin.onclick = function(){
-  idLogin.innerHTML = '<p><br><center>Niepoprawne haslo!<br>Sprobuj ponownie.</center> </p>';
-}
